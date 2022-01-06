515e9f2af7c550e05ecde8b87d140a6da1fd5cb5
Satyajit Ghosh <satyajit.edu@outlook.com>
2022-01-06 23:29:38 +0530

    Ui update #3

--- app/src/main/java/com/satyajitghosh/todoapp/CustomAdapter.java
+++ app/src/main/java/com/satyajitghosh/todoapp/CustomAdapter.java
@@ -1,6 +1,10 @@
 package com.satyajitghosh.todoapp;
 
 import android.content.Context;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +16,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class CustomAdapter extends ArrayAdapter<DataBaseModel> {
     private DBHandler dbHandler;
@@ -31,9 +36,11 @@ public class CustomAdapter extends ArrayAdapter<DataBaseModel> {
         }
         TextView textView1 = currentItemView.findViewById(R.id.titleView);
         textView1.setText(arrayList.get(position).getTitle());
-
-       // TextView textView2 = currentItemView.findViewById(R.id.impView);
-       // textView2.setText(arrayList.get(position).getImp());
+        int value=Integer.parseInt(arrayList.get(position).getImp());
+        if(value==1){
+            currentItemView.setBackgroundResource(R.drawable.rounded_corner_important);
+       }
+        Log.d("ImportantStatus",arrayList.get(position).getImp());
 
         Button button=currentItemView.findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
@@ -41,7 +48,8 @@ public class CustomAdapter extends ArrayAdapter<DataBaseModel> {
             public void onClick(View view) {
                 dbHandler = new DBHandler(getContext());
                 dbHandler.deleteItems(arrayList.get(position).getTitle());
-                arrayList.remove(position);
+              // arrayList.remove(position);
+                textView1.setPaintFlags(textView1.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
                 notifyDataSetChanged();
             }
         });
